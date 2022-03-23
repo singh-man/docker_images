4041d00d24e5fc3abb11eba4b016d3a393bdd437
Manish Singh <prataponandroid@gmail.com>
2022-03-23 14:57:38 +0000

    Added host user support for all, use ubuntu for nvim and alpine for others

--- fedora/install/40-neovim.sh
+++ fedora/install/40-neovim.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.config/nvim
 git clone https://github.com/singh-man/nvim-IDE.git ~/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-#nvim --headless --cmd PlugInstall +q
+#nvim --headless --cmd +PlugInstall +q
